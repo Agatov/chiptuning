d31575fddba9e5c0871b91382765d777088b56fd
agatov <agatovs@gmail.com>
2014-04-23 15:32:59 +0400

    initial commit

---
+++ assets/javascripts/google_maps.js
@@ -0,0 +1,41 @@
+/*Google Maps*/
+function initialize() {
+    var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+//    var featureOpts = [ { "stylers": [ { "saturation": -100 } ] } ];
+//    var styledMap = new google.maps.StyledMapType(featureOpts,{name: "Styled Map"});
+
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(55.688913, 37.437945),
+        disableDefaultUI: true,
+        scrollwheel: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, ]
+        }
+    };
+    var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+
+
+    var Marker = new google.maps.Marker({
+        position: new google.maps.LatLng(55.688913, 37.437945),
+        map: map,
+        icon: {
+            url: '/images/marker.png',
+            size: new google.maps.Size(67,98),
+            origin: new google.maps.Point(0,0),
+            anchor: new google.maps.Point(35, 95)
+        }
+    });
+//    map.mapTypes.set('map_style', styledMap);
+//    map.setMapTypeId('map_style');
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
